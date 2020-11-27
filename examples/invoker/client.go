782769baf6a689a4756517fcc1f9c9b8010c1714
Dmitrii Ustiugov <dmitrii.ustiugov@epfl.ch>
2020-11-28 00:04:54 +0100

    added stats to the simple invoker
    Signed-off-by: Dmitrii Ustiugov <dmitrii.ustiugov@epfl.ch>

--- examples/invoker/client.go
+++ examples/invoker/client.go
@@ -28,6 +28,7 @@ import (
 	"flag"
 	"fmt"
 	"os"
+	"sync/atomic"
 	"time"
 
 	log "github.com/sirupsen/logrus"
@@ -35,9 +36,10 @@ import (
 	"google.golang.org/grpc"
 )
 
+var completed int64
+
 func main() {
 	urlFile := flag.String("urlFile", "urls.txt", "File with functions' URLs")
-
 	rps := flag.Int("rps", 1, "Target requests per second")
 	runTime := flag.Int("time", 5, "Run the benchmark for X seconds")
 
@@ -68,16 +70,22 @@ func readLines(path string) ([]string, error) {
 	return lines, scanner.Err()
 }
 
-func runBenchmark(rps, runTime int, urls []string) {
+func runBenchmark(targetRPS, runTime int, urls []string) {
 	timeout := time.After(time.Duration(runTime) * time.Second)
-	tick := time.Tick(time.Duration(1000/rps) * time.Millisecond)
+	tick := time.Tick(time.Duration(1000/targetRPS) * time.Millisecond)
 
 	var issued int
+	start := time.Now()
 
 	for {
 		select {
 		case <-timeout:
+			duration := time.Since(start).Seconds()
+			realRPS := float64(completed) / duration
+			log.Infof("Real / target RPS : %.2f / %v", realRPS, targetRPS)
+
 			log.Println("Benchmark finished!")
+
 			return
 		case <-tick:
 			url := urls[issued%len(urls)]
@@ -89,7 +97,9 @@ func runBenchmark(rps, runTime int, urls []string) {
 }
 
 func invokeFunction(url string) {
-	address := fmt.Sprintf("%s:%d", url, 8080)
+	defer getDuration(startMeasurement(url)) // measure entire invocation time
+
+	address := fmt.Sprintf("%s:%d", url, 80)
 
 	conn, err := grpc.Dial(address, grpc.WithInsecure(), grpc.WithBlock())
 	if err != nil {
@@ -99,13 +109,23 @@ func invokeFunction(url string) {
 
 	c := pb.NewGreeterClient(conn)
 
-	ctx, cancel := context.WithTimeout(context.Background(), 20*time.Second)
+	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
 	defer cancel()
 
 	_, err = c.SayHello(ctx, &pb.HelloRequest{Name: "faas"})
 	if err != nil {
-		log.Warn("Failed to invoke ", address)
+		log.Warnf("Failed to invoke %v, err=%v", address, err)
 	}
 
+	atomic.AddInt64(&completed, 1)
+
 	return
+}
+
+func startMeasurement(msg string) (string, time.Time) {
+	return msg, time.Now()
+}
+
+func getDuration(msg string, start time.Time) {
+	log.Infof("%v,%v\n", msg, time.Since(start).Milliseconds())
 }
