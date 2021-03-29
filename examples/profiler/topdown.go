0a31fc814efe08853323266660f43a16bb2abed5
NiuJ1ao <26167136+NiuJ1ao@users.noreply.github.com>
2021-03-30 03:31:44 +0800

    create a topdown script
    Signed-off-by: NiuJ1ao <26167136+NiuJ1ao@users.noreply.github.com>

---
+++ examples/profiler/topdown.go
@@ -0,0 +1,58 @@
+// MIT License
+//
+// Copyright (c) 2021 Yuchen Niu and EASE lab
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+package main
+
+import (
+	"fmt"
+	"os"
+	"os/exec"
+	"strconv"
+)
+
+func main() {
+	var (
+		vmStep   = 2
+		maxVMs   = 32
+		cpuID    = 31
+		socket   = 1
+		funcs    = "pyaes"
+		dirs     = []string{"l1", "fb", "fl", "br", "bb", "mb", "cb", "l3", "l1", "db"}
+		nodeList = []string{"+IPC", "!+Frontend_Bound*/2,+MUX", "!+Fetch_Latency*/3,+MUX", "!+Branch_Resteers*/4,+MUX",
+			"!+Backend_Bound*/2,+MUX", "!+Memory_Bound*/3,+MUX", "!+Core_Bound*/3,+MUX", "!+L3_Bound*/4,+MUX", "!+L1_Bound*/5,+MUX", "!+DRAM_Bound*/5,+MUX"}
+	)
+
+	// run
+	for i, nodes := range nodeList {
+		numactl := exec.Command("numactl", "--cpunodebind=0", "--")
+		test := exec.Command("go", "test", "-v", "-timeout", "99999s", "-run", "TestProfileIncrementConfiguration", "-args", "-funcNames", funcs, "-vmIncrStep", strconv.Itoa(vmStep), "-maxVMNum", strconv.Itoa(maxVMs), "-bindSocket", strconv.Itoa(socket), "-profileCPUID", strconv.Itoa(cpuID), "-nodes", nodes, "-benchDirTest", dirs[i])
+		numactl.Args = append(numactl.Args, test.Args...)
+		numactl.Stdin = os.Stdin
+		numactl.Stdout = os.Stdout
+		numactl.Stderr = os.Stdout
+		fmt.Println("Command: ", numactl)
+		if err := numactl.Run(); err != nil {
+			fmt.Println(err)
+			return
+		}
+	}
+}
